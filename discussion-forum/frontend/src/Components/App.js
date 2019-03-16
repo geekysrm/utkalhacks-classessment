93c25d797e47d14ddc527cff9408dede6c105e8b
Bishal Subhadarshi Jena <bishal5058prime@gmail.com>
2019-03-16 11:53:06 +0530

    Add forum-discussion to project

---
+++ discussion-forum/frontend/src/Components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Router, Route, Switch } from "react-router-dom";
+
+
+const App = () => {
+    return (
+        <div> 
+            App
+        </div>
+        )
+}
+
+export default App;
